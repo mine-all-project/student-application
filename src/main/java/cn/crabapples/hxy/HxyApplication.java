101ac67a6a2d4094975c6ad93d5de53e75ee1a9d
quan <wishforyou.xia@gmail.com>
2019-09-10 01:28:57 +0800

    create database

--- src/main/java/cn/crabapples/hxy/HxyApplication.java
+++ src/main/java/cn/crabapples/hxy/HxyApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HxyApplication {
     public static void main(String[] args) {
         SpringApplication.run(HxyApplication.class, args);
-        System.err.println("欢迎使用遵义医科大学双选会系统");
+        System.err.println("启动成功");
     }
 }
