2ef54828fb0514b29b052b56a97dc8f1a3da44a1
crabapple <wishforyou.xia@gmail.com>
2020-04-02 00:10:25 +0800

    基础框架搭建

--- src/main/java/cn/crabapples/tuole/controller/IndexController.java
+++ src/main/java/cn/crabapples/tuole/controller/IndexController.java
@@ -2,7 +2,11 @@ package cn.crabapples.tuole.controller;
 
 import cn.crabapples.tuole.entity.User;
 import cn.crabapples.tuole.service.IndexService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,25 +20,42 @@ public class IndexController extends BaseHandle {
         this.indexService = indexService;
     }
 
-    @RequestMapping("/")
-    public String toLogin(){
-        return "login";
+    private Logger logger = LoggerFactory.getLogger(IndexController.class);
+    private final String PAGE = "page/";
+
+    @GetMapping("/page/{pageName}")
+    public String page(@PathVariable("pageName") String pageName) {
+        logger.info("收到请求->进入页面[{}]", pageName);
+        return PAGE + pageName;
     }
+
+    @GetMapping("/page/{dir}/{pageName}")
+    public String dirPage(@PathVariable("dir") String dir, @PathVariable("pageName") String pageName) {
+        logger.info("收到请求->进入二级页面[{}/{}]", dir, pageName);
+        return PAGE + dir + "/" + pageName;
+    }
+
+    @RequestMapping("/")
+    public String toLogin() {
+        return "index";
+    }
+
     @RequestMapping("login")
-    public String login(HttpServletRequest request, User user){
-        if (indexService.login(request, user)){
+    public String login(HttpServletRequest request, User user) {
+        if (indexService.login(request, user)) {
             User _this = (User) request.getSession().getAttribute("user");
-            if(checkUser(_this)){
+            if (checkUser(_this)) {
                 return "studentIndex";
-            }else{
+            } else {
                 return "orgIndex";
             }
         }
         return "";
     }
+
     @RequestMapping("welcome")
-    public String welcome(HttpServletRequest request){
-        request.setAttribute("welcome","欢迎使用遵义医科大学双选会系统!");
+    public String welcome(HttpServletRequest request) {
+        request.setAttribute("welcome", "欢迎使用遵义医科大学双选会系统!");
         return "welcome";
     }
 }
