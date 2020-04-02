83469f09e3b7a1b99cc9d717adf8bd8d0ca7dc00
hequan <hequan@gogpay.cn>
2020-04-02 11:06:01 +0800

    change application.java

--- src/main/java/cn/crabapples/tuole/StudentLeApplication.java
+++ src/main/java/cn/crabapples/tuole/StudentLeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TuoLeApplication {
+public class StudentLeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TuoLeApplication.class, args);
+        SpringApplication.run(StudentLeApplication.class, args);
     }
 }
