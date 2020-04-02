e2b7fb36f93d390e62f7b7e1ab3ceaf29ca9ed10
crabapple <wishforyou.xia@gmail.com>
2020-04-03 01:37:19 +0800

    调整部分二级页面为一级页面

--- src/main/java/cn/crabapples/tuole/config/ResourceConfigure.java
+++ src/main/java/cn/crabapples/tuole/config/ResourceConfigure.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * TODO
  *
  * @author Mr.He
- * 2020/4/2 21:54
+ * 2020/4/3 1:12
  * e-mail crabapples.cn@gmail.com
  * qq 294046317
  * pc-name 29404
@@ -21,8 +21,6 @@ public class ResourceConfigure implements WebMvcConfigurer {
 
     /**
      * 虚拟路径配置
-     *
-     * @param registry
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
