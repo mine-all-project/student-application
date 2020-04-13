83fad8b22777d14309392b9f2e82b69dcf2c3dd4
crabapple <wishforyou.xia@gmail.com>
2020-04-14 01:18:39 +0800

    项目初始化

---
+++ src/main/java/cn/crabapples/yangjichang/config/MailUtilsConfigure.java
@@ -0,0 +1,35 @@
+package cn.crabapples.yangjichang.config;
+
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * TODO 邮件配置类
+ *
+ * @author Mr.He
+ * 2020/3/20 21:15
+ * e-mail crabapples.cn@gmail.com
+ * qq 294046317
+ * pc-name 29404
+ */
+@Getter
+@Setter
+public class MailUtilsConfigure {
+    private String host = "smtp.qq.com";
+    private String source = "294046317@qq.com";
+    private String port = "465";
+    private String username = "294046317@qq.com";
+    private String password = "LILI5201";;
+    private boolean isAuth = true;
+    private boolean isDebug = true;
+    private String protocol = "smtp";
+    private String socketFactory = "javax.net.ssl.SSLSocketFactory";
+    private String[] targets = {"162165436@qq.com"};
+
+    @Override
+    public String toString() {
+        return JSONObject.toJSONString(this);
+    }
+}
---
+++ src/main/java/cn/crabapples/yangjichang/dao/OrderRepository.java
@@ -0,0 +1,24 @@
+package cn.crabapples.yangjichang.dao;
+
+import cn.crabapples.yangjichang.entity.Orders;
+import cn.crabapples.yangjichang.entity.SysUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * TODO 订单信息持久层
+ *
+ * @author Mr.He
+ * 2019/7/4 1422:51
+ * e-mail crabapples.cn@gmail.com
+ * qq 294046317
+ * pc-name 29404
+ */
+@Repository
+public interface OrderRepository extends JpaRepository<Orders, String> {
+
+    List<Orders> findAllBySysUserAndOrderTime(SysUser sysUser, LocalDate now);
+}
