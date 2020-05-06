d02d505e56c5f33c484b1337233ff82e229f2a4a
MrHe <crabapples.cn@gmail.com>
2020-05-07 00:15:03 +0800

    修改密码form

--- src/main/java/org/example/fangwuzulin/form/UserForm.java
+++ src/main/java/org/example/fangwuzulin/form/UserForm.java
@@ -21,17 +21,27 @@ public class UserForm {
     private String username;
 
     @Length(max = 32, message = "长度错误", groups = {IsCheckLength.class, IsLogin.class})
-    @NotBlank(message = "密码不能为空", groups = {IsNotNull.class, IsLogin.class, IsRegister.class})
+    @NotBlank(message = "密码不能为空", groups = {IsNotNull.class, IsLogin.class, IsEdit.class, IsRegister.class})
     private String password;
 
     @Length(max = 32)
     @NotBlank(message = "姓名不能为空", groups = {IsAdd.class, IsEdit.class, IsRegister.class})
     private String name;
 
+    @Length(max = 32)
+    @NotBlank(message = "电话不能为空", groups = {IsAdd.class, IsEdit.class, IsRegister.class})
+    private String phone;
+
+    @Length(max = 32)
+    @NotBlank(message = "邮箱不能为空", groups = {IsAdd.class, IsEdit.class, IsRegister.class})
+    private String mail;
+
     public SysUser toEntity() {
         SysUser user = new SysUser();
         user.setId(this.id);
         user.setName(this.name);
+        user.setPhone(this.phone);
+        user.setMail(this.mail);
         user.setUsername(this.username);
         user.setPassword(this.password);
         return user;
