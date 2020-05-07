57f7a96f05acde689bc31d9a36482d2d5228063d
MrHe <crabapples.cn@gmail.com>
2020-05-08 02:35:40 +0800

    管理后台页面测试

--- src/main/resources/static/vue/manage/main.js
+++ src/main/resources/static/vue/manage/main.js
@@ -1,17 +1,7 @@
-// 0. 如果使用模块化机制编程，导入Vue和VueRouter，要调用 Vue.use(VueRouter)
-
-// 1. 定义 (路由) 组件。
-// 可以从其他文件 import 进来
 Vue.use(httpVueLoader);
-
-// 2. 定义路由
-// 每个路由应该映射一个组件。 其中"component" 可以是
-// 通过 Vue.extend() 创建的组件构造器，
-// 或者，只是一个组件配置对象。
-// 我们晚点再讨论嵌套路由。
 const routes = [
     {path: '/demo', component: httpVueLoader('/demo-manage.vue')},
-    {path: '/manage/about', component: httpVueLoader('/vue/manage/about.vue')},
+    {path: '/manage/house-list', component: httpVueLoader('/vue/manage/house-list.vue')},
     {path: '/manage/culture', component: httpVueLoader('/vue/manage/culture.vue')},
     {path: '/manage/honor', component: httpVueLoader('/vue/manage/honor.vue')},
 
@@ -21,21 +11,12 @@ const routes = [
     {path: '/manage/products', component: httpVueLoader('/vue/manage/products.vue')},
     {path: '/manage/contact', component: httpVueLoader('/vue/manage/contact.vue')},
     {path: '/manage/user-list', component: httpVueLoader('/vue/manage/user-list.vue')},
-    {path: '/manage/orders-list', component: httpVueLoader('/vue/manage/orders-list.vue')},
+    {path: '/manage/orders-list', component: httpVueLoader('/vue/manage/house-list.vue')},
 
 ]
-
-// 3. 创建 router 实例，然后传 `routes` 配置
-// 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
     routes // (缩写) 相当于 routes: routes
 })
-
-
-// 4. 创建和挂载根实例。
-// 记得要通过 router 配置参数注入路由，
-// 从而让整个应用都有路由功能
-
 const app = new Vue({
     el: '#app',
     router,
@@ -45,14 +26,14 @@ const app = new Vue({
             menus: [
                 {
                     id: '1',
-                    name: '公司简介',
+                    name: '房源管理',
                     icon: 'el-icon-message',
                     children: [
                         {
                             id: '1-1',
-                            name: '关于我们',
+                            name: '房源列表',
                             icon: '',
-                            url: '/manage/about'
+                            url: '/manage/house-list'
                         },
                         {
                             id: '1-2',
