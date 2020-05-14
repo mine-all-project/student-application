8253a2cfb2f6ef58a37fc259359e66199c45deca
MrHe <crabapples.cn@gmail.com>
2020-05-15 06:44:16 +0800

    调整页面底部高度

--- src/main/resources/static/vue/portal/page-footer.vue
+++ src/main/resources/static/vue/portal/page-footer.vue
@@ -1,20 +1,21 @@
 <template>
 	<div>
-		<footer class="revealed">
+		<footer class="revealed" style="position: absolute;bottom: 0;
+			width: 100%;padding-top: 10px">
 			<div class="container">
-				<div class="row add_bottom_25">
+				<div class="row add_bottom_251">
 					<div class="col-lg-6">
 						<ul class="footer-selector clearfix">
-							<li>
-								123
-							</li>
+							<!--							<li>-->
+							<!--								123-->
+							<!--							</li>-->
 						</ul>
 					</div>
 					<div class="col-lg-6">
 						<ul class="additional_links">
-							<li><a href="#0">Terms and conditions</a></li>
-							<li><a href="#0">Privacy</a></li>
-							<li><span>© 2020 Allaia</span></li>
+							<!--							<li><a>Terms and conditions</a></li>-->
+							<!--							<li><a>Privacy</a></li>-->
+							<li><span>© 2020 房屋出租平台</span></li>
 						</ul>
 					</div>
 				</div>
