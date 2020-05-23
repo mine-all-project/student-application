90393d7279105ac40ef0ff9ded01fd65b35b637e
MrHe <crabapples.cn@gmail.com>
2020-05-23 11:11:29 +0800

    移除nacos，加入fastjson

--- src/main/java/org/example/gongjiao/StudentApplication.java
+++ src/main/java/org/example/gongjiao/StudentApplication.java
@@ -1,8 +1,5 @@
 package org.example.gongjiao;
 
-import com.alibaba.nacos.api.annotation.NacosProperties;
-import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +10,9 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "192.168.3.20:8848"))
-@NacosPropertySource(dataId = "gongjiao-dev.properties", autoRefreshed = true)
 public class StudentApplication {
     private static final Logger logger = LoggerFactory.getLogger(StudentApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
         logger.info("项目启动完成-->>");
