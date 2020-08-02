b757d29ca060803172505723ef78a20c2c4a5f22
crabapples <crabapples.cn@gmail.com>
2020-08-03 04:13:37 +0800

    订单查询

--- src/main/java/com/example/demo/ChromeApplication.java
+++ src/main/java/com/example/demo/ChromeApplication.java
@@ -5,27 +5,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ChromeApplication {
     public static void main(String[] args) {
-        String baseUrl = "https://www.yunpian.com/entry/register";
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+        String baseUrl = "https://www.baidu.com";
+        System.setProperty("webdriver.chrome.driver", "d:\\developer\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(baseUrl + "/");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        WebElement inputBox = driver.findElement(By.className("rect"));
+        WebElement inputBox = driver.findElement(By.id("kw"));
+        inputBox.sendKeys("测试");
+        WebElement searchButton = driver.findElement(By.id("su"));
+        searchButton.click();
+        List<WebElement> a = driver.findElements(By.className("t"));
+        a.forEach(e->{
+            System.err.println(e.getText());
+        });
+        a.get(5).findElement(By.tagName("a")).click();
 //        Assert.assertTrue(inputBox.isDisplayed());
-        inputBox.sendKeys("123");
-        WebElement verify = driver.findElement(By.className("verify-iconfont"));
-        verify.click();
-        WebElement slider = driver.findElement(By.className("yp-riddler-slider-btn"));
-        Actions actions = new Actions(driver);
-        actions.dragAndDropBy(slider,100,0).perform();
+//        inputBox.sendKeys("123");
+//        WebElement verify = driver.findElement(By.className("verify-iconfont"));
+//        verify.click();
+//        WebElement slider = driver.findElement(By.className("yp-riddler-slider-btn"));
+//        Actions actions = new Actions(driver);
+//        actions.dragAndDropBy(slider, 100, 0).perform();
     }
 }
